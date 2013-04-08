f9d4da4f29b929f5b08bf4f271a4dfc21b397ec9
Christopher Almquist <somethings90@gmail.com>
2013-04-08 17:46:30 -0600

    Added a short explanation

--- Array.h
+++ Array.h
@@ -1,3 +1,12 @@
+/*    
+    I designed this array class because I could and because I might need it for an
+    odd school project. It has a similar interfact to the standard array, but it
+    does not yet have the ability to initialize with an initializer list. The
+    initializer list support may or may not be added in the future.
+    
+    Copyright Christopher Almquist 2013
+*/
+
 #ifndef ARRAY_H
 #define ARRAY_H
 
